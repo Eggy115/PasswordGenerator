ac08780675a1b4bbab727a6c7f18943118cbb9e9
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2021-03-23 19:35:30 +0000

    Create Password Generator.py

---
+++ Password Generator.py
@@ -0,0 +1,26 @@
+from random import *
+import os
+
+os.system("@echo off")
+os.system("title Eggy115's Password Generator")
+os.system("color 0a")
+os.system("cls")
+
+lettercount = input("How many letters do you want in your password? ")
+
+password = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u','v', 
+            'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8','9', 
+            ':', ';', '@', '#', '~', '/', '?', '.', ',','<','>', 
+            '!', '$', '%', '^','&', '*', '(', ')', '{', '}', '[', ']', '-', '+', '=', '_', ' ','\\', '|', '`', '¬', '"', '£']
+
+passw = ""
+
+for key in range(int(lettercount)):
+    passw_letter = password[randint(0, 68)]
+    passw = str(passw_letter) + str(passw)
+
+os.system("color 0a")
+os.system("cls")
+print("Your randomly generated password is:\n" + passw)
+os.system("pause >nul")
